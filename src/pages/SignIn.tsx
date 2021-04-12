eaa04eedf2d934ce5dffa745f4a421c4a3827f59
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-12 17:50:10 +0500

    Transfer sign in logic from App to SignIn

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,33 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "../styles/SignIn.module.sass";
 import logo from "../img/mindLogo.svg";
 import ill from "../img/leftSideSVG.svg";
 import darkModeIcon from "../img/darkMode.svg";
+import { useHttp } from "../hooks/http.hook";
+import { UserRecord } from "../common/UserRecord.interface";
+import { Redirect } from "react-router-dom";
 
 export const SignIn: React.FC = () => {
+  const req = useHttp();
+  const [userData, setUserData] = useState<UserRecord | null>();
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [password, setPassword] = useState();
+
+  const handleInput = (event: any, type: string) =>
+    type === "phoneNumber" ?
+      setPhoneNumber(event.target.value) :
+      setPassword(event.target.value);
+
+  const handleSubmit = () => {
+    req<UserRecord>({
+      url: "http://localhost:5000/user/signIn",
+      method: "POST",
+      body: JSON.stringify(
+        {
+          phoneNumber: phoneNumber,
+          password: password
+        }
+      ),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then(result => {
+        if (!result.isExpected) {
+          // TODO: Error handler
+          return;
+        }
+        setUserData(result.value);
+      });
+  };
   return (
-    <div className={s.wrapper}>
-      <div className={s.leftSide}>
-        <img className={s.mindLogo} src={logo} alt="" />
-        <img className={s.illustration} src={ill} alt="" />
-        <img className={s.modeIcon} src={darkModeIcon} alt=""/>
-      </div>
-      <div className={s.rightSide}>
-        <div className={s.form}>
-          <p className={s.formTitle}>Умная парковочная система</p>
-          <p className={s.formSubtitle}>Еще не зарегистрированы?</p>
-          <div className={s.formInputs}>
-            <input type="text" className={s.formInput} placeholder="+7" />
-            <input type="password" className={s.formInput} placeholder="Пароль" />
+    <>
+      {userData && <Redirect push to="/home" />}
+      <div className={s.wrapper}>
+        <div className={s.leftSide}>
+          <img className={s.mindLogo} src={logo} alt="" />
+          <img className={s.illustration} src={ill} alt="" />
+          <img className={s.modeIcon} src={darkModeIcon} alt=""/>
+        </div>
+        <div className={s.rightSide}>
+          <div className={s.form}>
+            <p className={s.formTitle}>Умная парковочная система</p>
+            <p className={s.formSubtitle}>Еще не зарегистрированы?</p>
+            <div className={s.formInputs}>
+              <input type="text" className={s.formInput} placeholder="+7" onChange={(event => handleInput(event, "phoneNumber"))} />
+              <input type="password" className={s.formInput} placeholder="Пароль" onChange={(event => handleInput(event, "password"))} />
+            </div>
+            <button className={s.formButton} onClick={handleSubmit}>
+              <span className={s.formButtonTitle}>
+                Войти
+              </span>
+            </button>
           </div>
-          <button className={s.formButton}>
-            <span className={s.formButtonTitle}>
-              Войти
-            </span>
-          </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
