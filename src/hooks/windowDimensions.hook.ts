cbab7ab6afeeb882eeb4baee8d756efa3347eed6
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-14 16:00:09 +0500

    Create useWindowDimensions hook

---
+++ src/hooks/windowDimensions.hook.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+interface WindowDimensions {
+  width: number;
+  height: number;
+}
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState<WindowDimensions>({ width: innerWidth, height: innerHeight });
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowDimensions({ width: innerWidth, height: innerHeight });
+    });
+  }, []);
+
+  return windowDimensions;
+};
