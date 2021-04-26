ac2f8aebd27366b5e4bce6f205a366321e1805fe
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-26 23:55:51 +0500

    Update routing
    Add SingUp page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { SignIn } from "./pages/SignIn";
-import { Home } from "./pages/Home";
+import { SignUp } from "./pages/SignUp";
 
 const App: React.FC = () => {
   return (
     <Switch>
+      <Route exact path="/signUp" component={SignUp} />
       <Route exact path="/signIn" component={SignIn} />
-      <Route exact path="/home" component={Home} />
       <Redirect from="/" to="/signIn" />
     </Switch>
   );
