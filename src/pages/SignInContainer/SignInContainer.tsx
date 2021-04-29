c56636e1659729a08e382fe81e68d0761b26d57f
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-29 21:53:49 +0500

    Update SignInMobile

--- src/pages/SignInContainer/SignInContainer.tsx
+++ src/pages/SignInContainer/SignInContainer.tsx
@@ -1,18 +1,15 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import { SignInDto } from "../../common/SignInDto";
 import { UserRecord } from "../../common/UserRecord.interface";
 import { useHttp } from "../../hooks/http.hook";
 import { useMode } from "../../hooks/mode.hook";
 import { useWindowDimensions } from "../../hooks/windowDimensions.hook";
-import { darkModeConfig, lightModeConfig } from "../../styles/ModeConfig";
 import { SignIn } from "./SignIn";
 import { SignInMobile } from "./SignInMobile";
 
 export const SignInContainer = () => {
   const req = useHttp();
-  const [mode, toggleMode] = useMode();
   const [userData, setUserData] = useState<UserRecord | null>();
   const [phoneNumber, setPhoneNumber] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -49,7 +46,7 @@ export const SignInContainer = () => {
       {useWindowDimensions().width > 760 ? (
         <SignIn handleInput={handleInput} handleSubmit={handleSubmit} />
       ) : (
-        <SignInMobile />
+        <SignInMobile handleInput={handleInput} handleSubmit={handleSubmit} />
       )}
     </>
   );
--- src/pages/SignInContainer/SignInMobile.tsx
+++ src/pages/SignInContainer/SignInMobile.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { 
-  Form, 
+import {
+  Form,
   Logo,
-  Wrapper, 
-  FormSubtitle, 
-  FormTitle, 
-  Inputs, 
-  AppearanceModeIcon 
+  Wrapper,
+  FormSubtitle,
+  FormTitle,
+  Inputs,
+  AppearanceModeIcon,
 } from "../../styles/SignInMobile.styles";
 import logoLight from "../../img/mindLogoLight.svg";
 import logoDark from "../../img/mindLogoDark.svg";
@@ -17,26 +17,44 @@ import { Input } from "../../components/Input/Input";
 import { Button } from "../../components/Button/Button";
 import lightModeIcon from "../../img/lightMode.svg";
 import darkModeIcon from "../../img/darkMode.svg";
+import { SignInProps } from "./SignInProps";
 
-export const SignInMobile = () => {
+export const SignInMobile: React.FC<SignInProps> = ({
+  handleInput,
+  handleSubmit,
+}) => {
   const [mode, toggleMode] = useMode();
   return (
-    <>
-      <ThemeProvider theme={mode === "Light" ? lightModeConfig : darkModeConfig}>
-        <Wrapper>
-          <Logo src={mode === "Light" ? logoLight : logoDark} />
-          <Form>
-            <FormTitle>Умная парковочная система</FormTitle>
-            <FormSubtitle>Еще не зарегистрированы?</FormSubtitle>
-            <Inputs>
-              <Input placeholder="+7" paddingLeft="80vw" height="65vh" fontSizeMobile="20px" />
-              <Input type="password" paddingLeft="80vw" height="65vh" fontSizeMobile="20px" placeholder="Пароль" />
-            </Inputs>
-          </Form>
-          <Button title="Войти" />
-          <AppearanceModeIcon src={mode === "Light" ? darkModeIcon : lightModeIcon} onClick={toggleMode} />
-        </Wrapper>
-      </ThemeProvider>  
-    </>
+    <ThemeProvider theme={mode === "Light" ? lightModeConfig : darkModeConfig}>
+      <Wrapper>
+        <Logo src={mode === "Light" ? logoLight : logoDark} />
+        <Form>
+          <FormTitle>Умная парковочная система</FormTitle>
+          <FormSubtitle>Еще не зарегистрированы?</FormSubtitle>
+          <Inputs>
+            <Input
+              placeholder="+7"
+              paddingLeft="80vw"
+              height="65vh"
+              fontSizeMobile="20px"
+              onChange={(e: any) => handleInput(e, "phoneNumber")}
+            />
+            <Input
+              type="password"
+              paddingLeft="80vw"
+              height="65vh"
+              fontSizeMobile="20px"
+              placeholder="Пароль"
+              onChange={(e: any) => handleInput(e, "password")}
+            />
+          </Inputs>
+        </Form>
+        <Button title="Войти" onClick={handleSubmit} />
+        <AppearanceModeIcon
+          src={mode === "Light" ? darkModeIcon : lightModeIcon}
+          onClick={toggleMode}
+        />
+      </Wrapper>
+    </ThemeProvider>
   );
 };
