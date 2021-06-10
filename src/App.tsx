07d7f846052ed3a9fe37adb582f968219c8a17e1
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-06-10 23:31:41 +0500

    Raised navbar
    Now navbar is not rendered when changing pages (Home, History)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import { useWindowDimensions } from "./hooks/windowDimensions.hook";
 import { HistoryContainer } from "./pages/HistoryContainer/HistoryContainer";
 import { HomeContainer } from "./pages/HomeContainer/HomeContainer";
 import { SignInContainer } from "./pages/SignInContainer/SignInContainer";
 import { SignUpContainer } from "./pages/SignUpContainer/SignUpContainer";
 
 const App: React.FC = () => {
+  const { width } = useWindowDimensions();
   return (
     <Switch>
       <Route exact path="/signUp" component={SignUpContainer} />
       <Route exact path="/signIn" component={SignInContainer} />
-      <Route exact path="/home" component={HomeContainer} />
-      <Route exact path="/history" component={HistoryContainer} />
+      <>
+        {width > 760 ? <Navbar /> : <Navbar />}
+        <Route exact path="/home" component={HomeContainer} />
+        <Route exact path="/history" component={HistoryContainer} />
+      </>
       <Redirect from="/" to="/signIn" />
     </Switch>
   );
--- src/pages/HistoryContainer/History.tsx
+++ src/pages/HistoryContainer/History.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import { ThemeProvider } from "styled-components";
 import { ParkingRecord } from "../../common/ParkingRecord.interface";
-import { Navbar } from "../../components/Navbar/Navbar";
 import { ParkingWidget } from "../../components/ParkingWidget/ParkingWidget";
 import { useDateFormater } from "../../hooks/dateFormater.hook";
 import { useMode } from "../../hooks/mode.hook";
@@ -15,7 +14,6 @@ export const History: FC<Props> = ({ parkings }) => {
   const modeConfig = useMode()[2];
   return (
     <ThemeProvider theme={modeConfig}>
-      <Navbar />
       <Wrapper>
         {parkings.map((parking) => {
           return (
