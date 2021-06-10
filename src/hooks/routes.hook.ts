895fd016a1a8331b5e9400cd309775e8a2774ed5
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-06-10 23:57:37 +0500

    Create routes.hook.ts

---
+++ src/hooks/routes.hook.ts
@@ -0,0 +1,9 @@
+export const useRoutes = () => {
+  return {
+    signIn: () => "/signIn",
+    signUp: () => "/signUp",
+    home: () => "/home",
+    history: () => "history",
+    startup: () => "/",
+  };
+};
