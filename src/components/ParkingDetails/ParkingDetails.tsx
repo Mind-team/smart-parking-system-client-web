6d89442bc49162dfa2fda3e414170ed561ffcc18
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-06-14 17:04:28 +0500

    Update usage dateFormater in ParkingDetails

--- src/components/ParkingDetails/ParkingDetails.tsx
+++ src/components/ParkingDetails/ParkingDetails.tsx
@@ -4,26 +4,17 @@ import { useDateFormater } from "../../hooks/dateFormater.hook";
 import { Content, Line, Wrapper } from "./ParkingDetails.styles";
 
 export const ParkingDetails: FC<{ parking: ParkingRecord }> = ({ parking }) => {
+  const entryData = useDateFormater(new Date(parking.entryCarTime));
+  const departureData = useDateFormater(new Date(parking.departureCarTime));
+
   return (
     <Wrapper>
       <Content>
         <Line>Паркинг: {parking.parkingTitle}</Line>
-        <Line>
-          Дата въезда:
-          {useDateFormater(new Date(parking.entryCarTime)).fullDate}
-        </Line>
-        <Line>
-          Дата выезда:
-          {useDateFormater(new Date(parking.departureCarTime)).fullDate}
-        </Line>
-        <Line>
-          Время въезда:
-          {useDateFormater(new Date(parking.entryCarTime)).fullTime}
-        </Line>
-        <Line>
-          Время выезда:
-          {useDateFormater(new Date(parking.departureCarTime)).fullTime}
-        </Line>
+        <Line>Дата въезда: {entryData.fullDate}</Line>
+        <Line>Дата выезда: {departureData.fullDate}</Line>
+        <Line>Время въезда: {entryData.fullTime}</Line>
+        <Line>Время выезда: {departureData.fullTime}</Line>
         <Line>Регистрационный знак транспорта: {parking.carPlate}</Line>
         <Line>Сумма: {parking.priceRub}₽</Line>
       </Content>
