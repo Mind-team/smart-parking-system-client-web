356d2ba1ae74a3e4c5f4f180f8d37ad7b31306bd
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-07-01 12:41:39 +0500

    Create new actions for sign in

--- src/store/action-creators/user.ts
+++ src/store/action-creators/user.ts
@@ -3,6 +3,22 @@ import { UserRecord } from "../../common/UserRecord.interface";
 import { UserAction, UserActionType } from "../reducers/userReducer";
 import { ServerResponse } from "../../common/ServerResponse.interface";
 
+const signInAPI = async(phoneNumber: string, password: string): Promise<ServerResponse<UserRecord>> => {
+  const body = JSON.stringify({
+    phoneNumber,
+    password,
+  });
+  const response = await fetch("http://localhost:5000/user/signIn", { // TODO: remove hard link
+    method: "POST",
+    body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const res: ServerResponse<UserRecord> = await response.json();
+  return res;
+};
+
 export const fetchUserData = () => {
   return async(dispatch: Dispatch<UserAction>): Promise<void> => {
     try {
@@ -15,18 +31,7 @@ export const fetchUserData = () => {
         dispatch({ type: UserActionType.NOT_AUTHORIZED });
         return;
       }
-      const body = JSON.stringify({
-        phoneNumber,
-        password,
-      });
-      const response = await fetch("http://localhost:5000/user/signIn", {
-        method: "POST",
-        body,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const res: ServerResponse<UserRecord> = await response.json();
+      const res = await signInAPI(phoneNumber, password);
       dispatch({
         type: UserActionType.FETCH_DATA_SUCCESS,
         payload: res.value as UserRecord,
@@ -45,5 +50,54 @@ export const logout = () => {
     localStorage.removeItem("phoneNumber");
     localStorage.removeItem("password");
     dispatch({ type: UserActionType.LOGOUT });
+  };
+};
+
+export const signIn = () => {
+  return async(dispatch: Dispatch<UserAction>): Promise<void> => {
+    try {
+      dispatch({ type: UserActionType.SIGN_IN });
+      const [phoneNumber, password] = [
+        localStorage.getItem("phoneNumber"),
+        localStorage.getItem("password"),
+      ];
+      if (!phoneNumber || !password) {
+        dispatch({
+          type: UserActionType.SIGN_IN_ERROR,
+          payload: "Fill the form",
+        });
+        return;
+      }
+      const res = await signInAPI(phoneNumber, password);
+      if (!res.isExpected) {
+        dispatch({ type: UserActionType.SIGN_IN_ERROR, payload: res.message });
+        logout()(dispatch);
+        return;
+      }
+      dispatch({
+        type: UserActionType.SIGN_IN_SUCCESS,
+        payload: res.value as UserRecord,
+      });
+    } catch (error) {
+      dispatch({
+        type: UserActionType.SIGN_IN_ERROR,
+        payload: "Some problems with the Internet",
+      });
+    }
+  };
+};
+
+export const checkLocalStorage = () => {
+  return async(dispatch: Dispatch<UserAction>): Promise<void> => {
+    dispatch({ type: UserActionType.CHECK_LOCAL_STORAGE });
+    const [phoneNumber, password] = [
+      localStorage.getItem("phoneNumber"),
+      localStorage.getItem("password"),
+    ];
+    if (phoneNumber && password) {
+      signIn()(dispatch);
+      return;
+    }
+    dispatch({ type: UserActionType.EMPTY_LOCAL_STORAGE });
   };
 };
--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,9 +1,11 @@
 import { UserRecord } from "../../common/UserRecord.interface";
 
+type Error = string;
+
 interface UserState {
   user: UserRecord | null;
   isLoading: boolean;
-  isError: [false] | [true, string];
+  isError: [false] | [true, Error];
   isAuth: boolean;
 }
 
@@ -15,11 +17,16 @@ const defaultState: UserState = {
 };
 
 export enum UserActionType {
-  "FETCH_DATA",
-  "FETCH_DATA_SUCCESS",
-  "FETCH_DATA_ERROR",
-  "NOT_AUTHORIZED",
-  "LOGOUT",
+  FETCH_DATA = "FETCH_DATA",
+  FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS",
+  FETCH_DATA_ERROR = "FETCH_DATA_ERROR",
+  NOT_AUTHORIZED = "NOT_AUTHORIZED",
+  LOGOUT = "LOGOUT",
+  SIGN_IN = "SIGN_IN",
+  SIGN_IN_SUCCESS = "SIGN_IN_SUCCESS",
+  SIGN_IN_ERROR = "SIGN_IN_ERROR",
+  CHECK_LOCAL_STORAGE = "CHECK_LOCAL_STORAGE",
+  EMPTY_LOCAL_STORAGE = "EMPTY_LOCAL_STORAGE",
 }
 
 interface FetchUserAction {
@@ -33,7 +40,7 @@ interface SuccessFetchUserAction {
 
 interface ErrorFetchUserAction {
   type: UserActionType.FETCH_DATA_ERROR;
-  payload: string;
+  payload: Error;
 }
 
 interface NotAuthorizedUserAction {
@@ -44,28 +51,82 @@ interface LogoutUserAction {
   type: UserActionType.LOGOUT;
 }
 
+interface SignInUserAction {
+  type: UserActionType.SIGN_IN;
+}
+
+interface SuccessSignInUserAction {
+  type: UserActionType.SIGN_IN_SUCCESS;
+  payload: UserRecord;
+}
+
+interface ErrorSignInUserAction {
+  type: UserActionType.SIGN_IN_ERROR;
+  payload: Error;
+}
+
+interface CheckLocalStorageUserAction {
+  type: UserActionType.CHECK_LOCAL_STORAGE;
+}
+
+interface EmptyLocalStorageUserAction {
+  type: UserActionType.EMPTY_LOCAL_STORAGE;
+}
+
 export type UserAction =
   | FetchUserAction
   | SuccessFetchUserAction
   | ErrorFetchUserAction
   | NotAuthorizedUserAction
   | LogoutUserAction
+  | SignInUserAction
+  | SuccessSignInUserAction
+  | ErrorSignInUserAction
+  | CheckLocalStorageUserAction
+  | EmptyLocalStorageUserAction;
 
 export const userReducer = (
   state = defaultState,
   action: UserAction
 ): UserState => {
   switch (action.type) {
-    case UserActionType.FETCH_DATA:
-      return { user: null, isLoading: true, isError: [false], isAuth: true };
-    case UserActionType.FETCH_DATA_SUCCESS:
-      return { user: action.payload, isLoading: false, isError: [false], isAuth: true };
-    case UserActionType.FETCH_DATA_ERROR:
-      return { user: null, isLoading: false, isError: [true, action.payload], isAuth: true };
-    case UserActionType.NOT_AUTHORIZED:
-      return { user: null, isLoading: false, isError: [true, "You are not auth"], isAuth: false };
+    case UserActionType.CHECK_LOCAL_STORAGE:
+    case UserActionType.EMPTY_LOCAL_STORAGE:
     case UserActionType.LOGOUT:
       return { user: null, isLoading: false, isError: [false], isAuth: false };
+    case UserActionType.FETCH_DATA:
+      return { user: null, isLoading: true, isError: [false], isAuth: true };
+    case UserActionType.SIGN_IN:
+      return { user: null, isLoading: true, isError: [false], isAuth: false };
+    case UserActionType.FETCH_DATA_SUCCESS:
+    case UserActionType.SIGN_IN_SUCCESS:
+      return {
+        user: action.payload,
+        isLoading: false,
+        isError: [false],
+        isAuth: true,
+      };
+    case UserActionType.FETCH_DATA_ERROR:
+      return {
+        user: null,
+        isLoading: false,
+        isError: [true, action.payload],
+        isAuth: true,
+      };
+    case UserActionType.SIGN_IN_ERROR:
+      return {
+        user: null,
+        isLoading: false,
+        isError: [true, action.payload],
+        isAuth: false,
+      };
+    case UserActionType.NOT_AUTHORIZED:
+      return {
+        user: null,
+        isLoading: false,
+        isError: [true, "You are not auth"],
+        isAuth: false,
+      };
     default:
       return state;
   }
