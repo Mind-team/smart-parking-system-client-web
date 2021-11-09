8f82f72ba7e8d1510ef7fbc584f6df3fa3524672
Ilya <Ermolaev.ID@yandex.ru>
2021-11-09 15:57:28 +0500

    deps: setup storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,22 @@
+import { ThemeProvider } from "styled-components";
+import { lightModeConfig, darkModeConfig } from "../src/styles/ModeConfig";
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ThemeProvider
+        theme={
+          localStorage.getItem("mode") === "light"
+            ? lightModeConfig
+            : darkModeConfig
+        }
+      >
+        <Story />
+      </ThemeProvider>
+    );
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +25,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
