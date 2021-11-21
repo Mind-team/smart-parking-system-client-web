c37ab55e1c7472d7bdebe32769f1ab0f77d01cf8
Ermolaev Ilya <Ermolaev.ID@yandex.ru>
2021-11-21 21:40:15 +0500

    ui(Global): background color changes

--- src/store/reducers/appearanceModeReducer.ts
+++ src/store/reducers/appearanceModeReducer.ts
@@ -18,8 +18,10 @@ export const appearanceModeReducer: Reducer<AppearanceModeState> = (
 ) => {
   switch (action.type) {
     case AppearanceModeActionType.SWITCH_TO_LIGHT_MODE:
+      document.body.style.backgroundColor = lightModeConfig.background;
       return { title: AppearanceMode.light, config: lightModeConfig };
     case AppearanceModeActionType.SWITCH_TO_DARK_MODE:
+      document.body.style.backgroundColor = darkModeConfig.background;
       return { title: AppearanceMode.dark, config: darkModeConfig };
     default:
       return state;
